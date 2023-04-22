839955f649e2f71317a15d79ba01ede2753f4aef
Avijit Karmaker <ajkacca@gmail.com>
2023-04-22 10:47:48 +0300

    update user model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import validator from "validator";
 
 
 const UserSchema = new mongoose.Schema({
@@ -6,14 +7,24 @@ const UserSchema = new mongoose.Schema({
     username: {
         type: String,
         requried: [true, "Please add user name"],
+        minLength:[4,"username cant be shorter than 4 character"]
     },
     email: {
         type: String,
-        match: []
+        unique:true,
+        required:[true,"email is required"],
+        validate: {
+            validator:validator.isEmail,
+            message:"provide a valid email"
+        }
     },
     password: {
         type: String,
-        requried: [true, "Please add password"],
+        required: [true, "Please add password"],
+        trim:true,
+        minLength:6,
+        maxLength:20,
+        select:false
     }
 
 });
