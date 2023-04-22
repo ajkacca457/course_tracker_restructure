c0ed210f094cdf5afb36baa7e0b23d89e3ac5691
Avijit Karmaker <ajkacca@gmail.com>
2023-04-22 14:01:33 +0300

    update error handling in course controlelr

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -29,7 +29,8 @@ export const getSingleCourse = AysncHanlder(async (req, res, next) => {
     
         res.status(200).json({
             success:true,
-            data:course
+            data:course,
+            message: `course found with ${req.params.id}`,
         }); 
 });
 
