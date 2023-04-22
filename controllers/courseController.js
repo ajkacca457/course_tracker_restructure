a1c271dc3b0449c4bd8938548f90fce58ffdac96
Avijit Karmaker <ajkacca@gmail.com>
2023-04-22 13:56:31 +0300

    add statuscode, update aysnc handler

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,13 +1,14 @@
 import Course from "../models/Course.js";
 import AsyncHandler from "../middlewares/AsyncHandler.js";
 import CustomError from "../utils/CustomError.js";
+import {StatusCodes} from "http-status-codes";
 
 export const getAllCourses = AsyncHandler(async (req, res,next) => {
 
     const courses = await Course.find();
 
     if (!courses) {
-        return (next(new CustomError("courses are not available",401)))
+        return (next(new CustomError("courses are not available",StatusCodes.BAD_REQUEST)))
     }
 
     res.status(200).json({
@@ -23,7 +24,7 @@ export const getSingleCourse = AysncHanlder(async (req, res, next) => {
         const course= await Course.findById(req.params.id);
 
         if(!course) {
-            return next(new CustomError(`course is not available with id: ${req.params.id}`))
+            return next(new CustomError(`course is not available with id: ${req.params.id}`,StatusCodes.BAD_REQUEST))
         }
     
         res.status(200).json({
@@ -37,7 +38,7 @@ export const createCourse = AsyncHandler(async (req, res,next) => {
     const { name, number_of_lessons, lesson_completed, hours_needed, hours_spended } = req.body;
 
     if (!name || !number_of_lessons || !lesson_completed || !hours_needed || !hours_spended) {
-        return res.status(400).json({ success: false, error: "Please provide all the fields" });
+        return next(new CustomError("Please provide all the fields", StatusCodes.BAD_REQUEST))
     }
 
     const course = await Course.create({ name, number_of_lessons, lesson_completed, hours_needed, hours_spended });
@@ -50,15 +51,15 @@ export const createCourse = AsyncHandler(async (req, res,next) => {
 });
 
 
-export const updateCourse = async (req, res) => {
+export const updateCourse = AsyncHandler(async (req, res, next) => {
     res.status(200).json({
         message: "This route will update a course"
     });
-};
+});
 
 
-export const deleteCourse = async (req, res) => {
+export const deleteCourse =AsyncHandler(async (req, res, next) => {
     res.status(200).json({
         message: "This route will delete a course"
     });
-};
+});
