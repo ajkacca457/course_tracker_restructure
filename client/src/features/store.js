42405d037b810056261d13764fc40fd01b116850
Avijit Karmaker <ajkacca@gmail.com>
2023-04-29 19:27:37 +0300

    add user slice

--- client/src/features/store.js
+++ client/src/features/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
 import courseSlice from "./slices/courseSlice";
+import userSlice from "./slices/userSlice";
 
 export const store= configureStore({
     reducer:{
-        courses:courseSlice
+        courses:courseSlice,
+        user:userSlice
     }
 })
