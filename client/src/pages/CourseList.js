dd97c722c8d1a530826ce8cf52c8f03093ab7ce5
Avijit karmaker <ajkacca@gmail.com>
2023-05-02 17:09:53 +0300

    update courselist

--- client/src/pages/CourseList.js
+++ client/src/pages/CourseList.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { toast } from "react-toastify";
 
 
 const CourseList = () => {
+
+
+
   return (
     <div>CourseList</div>
-  )
-}
+  );
+};
 
-export default CourseList
+export default CourseList;
