e5a3b534757a03138383c448d0ea2dd5eca34124
Avijit karmaker <ajkacca@gmail.com>
2023-05-02 10:05:18 +0300

    update icons

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { AiTwotoneHome } from "react-icons/ai";
+import { MdLibraryAdd } from "react-icons/md";
 
 const Navigation = () => (
   <div className="easynav">
     <div className="navcont text-white bg-dark">
 
       <div className="nav-item-easy">
-        <NavLink to="/dashboard" className="text-white">
-          <i className="fas fa-home" />
+        <NavLink to="/dashboard" className="text-white text-decoration-none">
+          <AiTwotoneHome />
           <p>Home</p>
         </NavLink>
       </div>
 
       <div className="nav-item-easy">
-        <NavLink to="/dashboard/add-course" className="text-white">
-          <i className="fas fa-plus-circle" />
+        <NavLink to="/dashboard/add-course" className="text-white text-decoration-none">
+          <MdLibraryAdd />
           <p>Add Course</p>
         </NavLink>
 
